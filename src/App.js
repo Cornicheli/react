c782fd73df2163aceb526bf8e6ff04d62ebc7bc3
cornicheli <gabrielcornide.pg@gmail.com>
2022-11-18 21:21:34 -0300

    memories

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg'; // es una funcion desde js
+import './App.css';
+//en la linea 8 se emplea directamente con llaves
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Gabriel Cornide
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
